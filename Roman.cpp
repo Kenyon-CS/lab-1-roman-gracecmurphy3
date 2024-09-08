91e5e88afd60ed3dc32eb3d02ac6c7094ad3b635
Grace Murphy <gracemurphy@Graces-MacBook-Air-3.local>
2024-09-08 19:37:26 -0400

    int to roman dictionary and converter

--- Roman.cpp
+++ Roman.cpp
@@ -24,6 +24,23 @@ int Roman::romanToInt(const std:: string& roman) {
     }
     return tot;
     }
+
+    std::string Roman::intToRoman(int Num) {
+        std::pair <int, std::string> romanPairs[] = {
+            {1000, "M"}, {900, "CM"}, {500, "D"},{400, "CD"}, {100, "C"}, 
+            {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"},
+            {4, "IV"}, {1, "I"}
+            };
+        std::string result = "";
+        for (const auto& pair : romanParis){
+            while (num >= pair.first){
+                result += pair.second;
+                num -= pair.first;
+            }
+        }
+          return result;  
+        }
+    }
 }
 
 
