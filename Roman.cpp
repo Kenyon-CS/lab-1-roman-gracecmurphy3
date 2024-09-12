4719ac4cba7e0954f758827d6d509700581dd08e
Grace Murphy <gracemurphy@Graces-MacBook-Air-3.local>
2024-09-12 11:52:42 -0400

    fully commented code

--- Roman.cpp
+++ Roman.cpp
@@ -1,27 +1,40 @@
-#include "roman.h"
+#include "Roman.h"
 #include <unordered_map>
+#include <iostream>
 
+using namespace std;
+// initializes an empty string for the roman numeral to fill and return as well as a 0 value for the integer return
 Roman::Roman() : romanNumeral(""), integerValue(0){}
-Roman::Roman(const std::string& roman) : romanNumeral(roman), integerValue(romanToInt(roman)){}
-Roman::Roman(int value) : integerValue(value), romanNumeral(intToRoman(value)){}
 
+// initializes variable to be used in romanToInt
+Roman::Roman(const string& roman){
+    romanNumeral = roman;
+    integerValue = romanToInt(roman);
+}
+//initializes variable to be used in intToRoman
+Roman::Roman(int value){
+    integerValue = value;
+    romanNumeral = intToRoman(value);
+}
+//calls and prints the roman numeral that getRoman returns
 void Roman::printRoman() const {
-    std::cout << romanNumeral << std::endl;
+    cout << romanNumeral << endl;
 }
-
-void Roman::printInteger() const {
-    std::cout << integerValue << std::endl;
+// calls and prints the value that getInt returns
+void Roman::printDec() const {
+    cout << integerValue << endl;
 }
-
-std::string Roman::getRoman() const {
+// calls getRoman function
+string Roman::getRoman() const {
     return romanNumeral;
 }
-
+// calls getInt function
 int Roman::getInt() const {
     return integerValue;
 }
-int Roman::romanToInt(const std:: string& roman) {
-    std::unordered_map<char, int> romanDict = {
+
+int Roman::romanToInt(const string& roman) {
+    unordered_map<char, int> romanDict = { //dictionary of all roman numerals and their numerical values
         {'I', 1}, {'i', 1}, {'V', 5}, {'v', 5}, 
         {'X', 10}, {'x',10}, {'L',50}, {'l',50},
         {'C',100}, {'c',100}, {'D',500}, {'d',500},
@@ -30,7 +43,7 @@ int Roman::romanToInt(const std:: string& roman) {
 
     int tot = 0;
     int prior = 0;
-
+// loops through the inputed roman numeral from the end to calculate its numerical value
     for (int i = roman.size() - 1; i>=0; --i){
         int value = romanDict[roman[i]];
         if (value < prior){
@@ -44,22 +57,22 @@ int Roman::romanToInt(const std:: string& roman) {
     return tot;
     }
 
-    std::string Roman::intToRoman(int Num) {
-        std::pair <int, std::string> romanPairs[] = {
+    string Roman::intToRoman(int num) {
+        pair <int, string> romanPairs[] = { // dictionary for the corresponding roman symbol for different numerical values
             {1000, "M"}, {900, "CM"}, {500, "D"},{400, "CD"}, {100, "C"}, 
             {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"},
             {4, "IV"}, {1, "I"}
             };
-        std::string result = "";
-        for (const auto& pair : romanParis){
+        string result = "";
+        for (const auto& pair : romanPairs){ // loops through the dictionary pairs to append values whern they're larger than the previous
             while (num >= pair.first){
                 result += pair.second;
                 num -= pair.first;
             }
         }
           return result;  
-        }
-    }
 }
+    
+
 
 
--- makefile
+++ makefile
@@ -3,13 +3,13 @@ CXX = g++
 CXXFLAGS = -Wall -std=c++11
 
 # Target executable
-TARGET = point_program
+TARGET = Roman_program
 
 # Source files
-SRCS = main.cpp Point.cpp
+SRCS = main.cpp Roman.cpp
 
 # Object files
-OBJS = main.o Point.o
+OBJS = main.o Roman.o
 
 # Default target
 all: $(TARGET)
@@ -19,12 +19,12 @@ $(TARGET): $(OBJS)
 	$(CXX) $(CXXFLAGS) -o $(TARGET) $(OBJS)
 
 # Rule to compile main.cpp into main.o
-main.o: main.cpp Point.h
+main.o: main.cpp Roman.h
 	$(CXX) $(CXXFLAGS) -c main.cpp
 
 # Rule to compile Point.cpp into Point.o
-Point.o: Point.cpp Point.h
-	$(CXX) $(CXXFLAGS) -c Point.cpp
+Roman.o: Roman.cpp Roman.h
+	$(CXX) $(CXXFLAGS) -c Roman.cpp
 
 # Clean up build files
 clean:
